de001ef9d431a20201ed26c90f362bfa5a35328a
Hector Rincon <hecerinc@hotmail.com>
2016-10-25 00:20:43 -0500

    Initial commit

---
+++ circuito/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  circuito
+//
+//  Created by Hector Rincon on 10/24/16.
+//  Copyright © 2016 Hector Rincon. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyA8Bw_-gAPgo__o2GHas7AzJnVuhg42r_g")
+        setStatusBarBackgroundColor(color: UIColor(red: 0/255, green: 118/255, blue: 235/255, alpha: 1.0))
+        return true
+    }
+    func setStatusBarBackgroundColor(color: UIColor) {
+        
+        guard let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView else { return }
+        
+        statusBar.backgroundColor = color
+    }
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ circuito/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  circuito
+//
+//  Created by Hector Rincon on 10/24/16.
+//  Copyright © 2016 Hector Rincon. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewMap: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        viewMap.isMyLocationEnabled = true
+        viewMap.camera = camera
+        
+        
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = viewMap
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
