116a17147190783c0e39624102332606e3e52fd0
Hector Rincon <hecerinc@hotmail.com>
2016-11-03 16:01:39 -0600

    Add ruta Garza Sada

--- circuito/ViewController.swift
+++ circuito/ViewController.swift
@@ -16,19 +16,32 @@ class ViewController: AppController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 25.651783081997173, longitude: -100.2932983, zoom: 14.0)
         //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         viewMap.isMyLocationEnabled = true
         viewMap.camera = camera
         
-        
+        let routePath = makeRoutePath()
+        let polyLine = GMSPolyline(path: routePath)
+        polyLine.strokeWidth = 5.0
+        polyLine.strokeColor = UIColor.blue
+        polyLine.map = viewMap
         
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 25.651431, longitude: -100.29106)
+        marker.title = "Tec de Monterrey"
+        marker.snippet = "Campus Monterrey"
         marker.map = viewMap
         
+    }
+    func makeRoutePath() -> GMSMutablePath {
+        let path = GMSMutablePath()
+        
+        for coord in mapCoords{
+            path.add(CLLocationCoordinate2D(latitude: coord.1, longitude: coord.0))
+        }
+        
+        return path
     }
 
     override func didReceiveMemoryWarning() {
