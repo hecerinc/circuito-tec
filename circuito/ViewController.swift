97011ec5b8d521538e2790ffde921f7bf1b6a4be
EliasMera <AndreaT@Andreas-MacBook-Pro.local>
2016-11-22 10:30:27 -0600

    Added call the web service periodically

--- circuito/ViewController.swift
+++ circuito/ViewController.swift
@@ -14,6 +14,9 @@ import AEXML
 class ViewController: AppController {
 
     @IBOutlet weak var viewMap: GMSMapView!
+    var arrMarksCircuito = [GMSMarker]()
+    // llama al web service cada 2 segundos
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +41,7 @@ class ViewController: AppController {
         marker.map = viewMap
         buildStops()
         
-        // llama al web service 
         uploadCircuito()
-        
     }
     func makeRoutePath() -> GMSMutablePath {
         let path = GMSMutablePath()
@@ -69,20 +70,56 @@ class ViewController: AppController {
     }
     
     // funcion para llamar al web service
-    func uploadCircuito(){
+    func uploadCircuito() -> Void {
+        scheduledTimerWithTimeInterval()
+        var arrLat = [Double]()
+        var arrLong = [Double]()
         Alamofire.request("http://ws.locatel.es/servicios/vehiculos/vehiculos.asmx/ListaVehiculos?usuario=tec1&password=tec1").responseString { response in
             print("Success: \(response.result.isSuccess)")
             //print("Response String: \(response.result.value!)")
             do {
                 let xmldoc = try AEXMLDocument(xml: response.result.value!)
+                for ult_latitud in xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_latitud"].all!{
+                    arrLat.append((ult_latitud.string as NSString).doubleValue)
+                }
+                for ult_longitutd in xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_longitud"].all!{
+                    arrLong.append((ult_longitutd.string as NSString).doubleValue)
+                }
                 
-                print(xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_latitud"].value!)
-                print(xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_longitud"].value!)
+                // primero elimina los markers existentes luego repinta
+                for i in 0...self.arrMarksCircuito.count-1 {
+                    self.arrMarksCircuito[i].map = nil
+                    print("borrado exitoso")
+                }
+                if self.arrMarksCircuito.count > 0 {
+                    self.arrMarksCircuito.removeAll()
+                }
+                
+                // repinta markers de circuito
+                for i in 0...arrLat.count-1 {
+                    print("agregado")
+                    print(arrLat[i])
+                    print(arrLong[i])
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: arrLat[i], longitude: arrLong[i])
+                    marker.title = "Circuito Tec"
+                    marker.snippet = "Localizacion real"
+                    marker.icon = GMSMarker.markerImage(with: UIColor.blue)
+                    marker.map = self.viewMap
+                    self.arrMarksCircuito.append(marker)
+                }
+                //print(xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_latitud"].value!)
+                //print(xmldoc.root["diffgr:diffgram"]["Flota"]["coches"]["ult_longitud"].value!)
                 //["DataSet"]["diffgr:diffgram"]["Flota"]["coches"]["ult_latitud"].value
             } catch{
                 print(error)
             }
         }
+    }
+    
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function **Countdown** with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.uploadCircuito), userInfo: nil, repeats: true)
     }
 
 
